3bc1963d7f0a63afdf8e64fb0fbf62e8bdba8576
Kapil Pant <kapilpant@gmail.com>
2025-07-28 01:10:04 +0100

    Update base path for GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import path from 'path'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: process.env.NODE_ENV === 'production' ? '/payserveme-website/' : '/',
+  base: process.env.NODE_ENV === 'production' ? '/payserveme_demo/' : '/',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
